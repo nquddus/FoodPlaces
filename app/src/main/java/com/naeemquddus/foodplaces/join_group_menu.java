e05ab53aca54a718b960f2562fea1f8c469d4bc6
lyeec9 <lyeec9@gmail.com>
2014-10-18 17:02:01 -0500

    Basic main menu implementation

--- app/src/main/java/com/naeemquddus/foodplaces/join_group_menu.java
+++ app/src/main/java/com/naeemquddus/foodplaces/join_group_menu.java
@@ -6,19 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MyActivity extends Activity {
+public class join_group_menu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_join_group_menu);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.join_group_menu, menu);
         return true;
     }
 
