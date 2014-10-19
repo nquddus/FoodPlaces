b119cec88373cbf9d0db6ee3056bb45653ea9f18
BrianShiau <brian-s@utexas.edu>
2014-10-18 20:11:38 -0500

    location code minus prompt to enable

--- app/src/main/java/com/naeemquddus/foodplaces/main_menu.java
+++ app/src/main/java/com/naeemquddus/foodplaces/main_menu.java
@@ -1,21 +1,50 @@
 package com.naeemquddus.foodplaces;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class main_menu extends Activity {
+public class main_menu extends Activity implements LocationListener {
+    private double latitudeField;
+    private double longitudeField;
+    private LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        setupGPS();
+
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the location provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            System.out.println("Location not available");
+        }
+        System.out.println("lat:" + latitudeField + " long:" + longitudeField);
     }
 
 
@@ -54,6 +83,44 @@ public class main_menu extends Activity {
         startActivity(intent);
     }
 
+    /* Request updates at startup */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    /* Remove the LocationListener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitudeField = location.getLatitude();
+        longitudeField = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
     public void setupGPS() {
         LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
         boolean enabled = service
@@ -65,5 +132,8 @@ public class main_menu extends Activity {
             Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
             startActivity(intent);
         }
+        Toast.makeText(this, "location enabled: "+enabled,
+                Toast.LENGTH_SHORT).show();
+        System.out.println("lat:" + latitudeField + " long:" + longitudeField);
     }
 }
